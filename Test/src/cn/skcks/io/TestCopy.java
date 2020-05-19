6eeda31cce2936b14fdfbee3962c6ed24747c851
shikong-sk <shikong-sk@users.noreply.github.com>
2020-05-20 03:41:55 +0800

    Update TestCopy.java

--- Test/src/cn/skcks/io/TestCopy.java
+++ Test/src/cn/skcks/io/TestCopy.java
@@ -8,11 +8,14 @@ import java.io.*;
  */
 public class TestCopy {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
         // 源文件
-        File src = new File("io/TestSteam_1");
+        File src = new File("io/TestStream_1");
+//        File src = new File("E:\\#编程语言速成\\learnxinyminutes-docs.zip");
 
         // 目标文件
-        File dest = new File("io/TestSteam_copy");
+        File dest = new File("io/TestStream_copy");
 
         // 自动资源管理
 //        try (
@@ -43,18 +46,20 @@ public class TestCopy {
             System.out.println("目标文件：" + dest.getAbsolutePath());
             System.out.println("=====  执行复制文件  =====");
 
-            byte[] flush = new byte[1024 / 2];
-            long fileLength = src.length();
+            int bufferedLen = 1024 / 2;
+            byte[] flush = new byte[bufferedLen];
             int readLen;
             while ((readLen = srcStream.read(flush)) != -1) {
                 destStream.write(flush, 0, readLen);
-                System.out.println("=====    " + String.format("%3.2f",fileLength / (double)readLen * 100) + "%    =====");
             }
 
             // 刷新文件缓存
             destStream.flush();
 
             System.out.println("=====  文件复制成功  =====");
+            long endTime = System.currentTimeMillis();
+            System.out.println("耗时：" + (endTime - startTime) + "ms");
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
